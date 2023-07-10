023ef50b013efa4fa27e460825d8bebc4a632c2b
med <med.elkhalki0@gmail.com>
2023-07-10 07:42:06 +0200

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testPathIgnorePatterns: ['node_modules/', 'dist/', 'build/'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'clover'],
 };
